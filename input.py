2b607c63800ff1db75dc31f52c785474ec0c1f9f
kelvin defrNBAH <kevinocholah@gmail.com>
2024-06-25 02:25:59 +0300

    plp python review

---
+++ input.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#ask the user for thei input
+name = input("Please enter your name: ")
+print("Hello, " + name)
+
+#ask the usr for their age
+age = input("How old are you? ")
+print("You are " + age + " years old")
+
+#ask the user for their location
+location = input("where are you from? ")
+print("You are from " + location)
